6306a1beacfaf717b0334560150aa869e833741a
Jaela Wesley <jcwesley93@gmail.com>
2019-02-05 05:39:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,10 @@ def my_each (array)
 
   counter = 0
 
-  while my_each(array) do |element|
+  while counter < array.length
+    yield (array[counter])
+    counter += 1
+    
   end
     array
 end
